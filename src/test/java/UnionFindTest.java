ddafc0add11f73aa2dc4d36c04f807e89eb071ac
mmulla4 <mmulla4@uic.edu>
2018-12-04 22:00:32 -0600

    Refactoring code

--- src/test/java/UnionFindTest.java
+++ src/test/java/UnionFindTest.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.*;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -8,10 +9,10 @@ public class UnionFindTest {
     UnionFind unionFind;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         data = new int[10];
-        for(int i=0;i<data.length;i++)
-            data[i]=i;
+        for (int i = 0; i < data.length; i++)
+            data[i] = i;
 
         unionFind = new UnionFind().makeSet(data);
 
@@ -37,19 +38,19 @@ public class UnionFindTest {
     //8. find(0) == find(1)
 
     @Test
-    public void unionFindAlgoTest(){
-        assertEquals(unionFind.find(unionFind.nodes[0]),unionFind.nodes[0]);
-        unionFind.union(unionFind.nodes[0],unionFind.nodes[3]);
-        assertEquals(unionFind.find(unionFind.nodes[0]),unionFind.find(unionFind.nodes[3]));
-        unionFind.union(unionFind.nodes[3],unionFind.nodes[6]);
-        assertEquals(unionFind.find(unionFind.nodes[0]),unionFind.find(unionFind.nodes[6]));
-        assertEquals(unionFind.find(unionFind.nodes[3]),unionFind.find(unionFind.nodes[6]));
-        unionFind.union(unionFind.nodes[1],unionFind.nodes[2]);
-        assertEquals(unionFind.find(unionFind.nodes[1]),unionFind.find(unionFind.nodes[2]));
-        assertNotEquals(unionFind.find(unionFind.nodes[0]),unionFind.find(unionFind.nodes[2]));
-        unionFind.union(unionFind.nodes[3],unionFind.nodes[1]);
-        assertEquals(unionFind.find(unionFind.nodes[3]),unionFind.find(unionFind.nodes[2]));
-        assertEquals(unionFind.find(unionFind.nodes[0]),unionFind.find(unionFind.nodes[1]));
+    public void unionFindAlgoTest() {
+        assertEquals(unionFind.find(unionFind.nodes[0]), unionFind.nodes[0]);
+        unionFind.union(unionFind.nodes[0], unionFind.nodes[3]);
+        assertEquals(unionFind.find(unionFind.nodes[0]), unionFind.find(unionFind.nodes[3]));
+        unionFind.union(unionFind.nodes[3], unionFind.nodes[6]);
+        assertEquals(unionFind.find(unionFind.nodes[0]), unionFind.find(unionFind.nodes[6]));
+        assertEquals(unionFind.find(unionFind.nodes[3]), unionFind.find(unionFind.nodes[6]));
+        unionFind.union(unionFind.nodes[1], unionFind.nodes[2]);
+        assertEquals(unionFind.find(unionFind.nodes[1]), unionFind.find(unionFind.nodes[2]));
+        assertNotEquals(unionFind.find(unionFind.nodes[0]), unionFind.find(unionFind.nodes[2]));
+        unionFind.union(unionFind.nodes[3], unionFind.nodes[1]);
+        assertEquals(unionFind.find(unionFind.nodes[3]), unionFind.find(unionFind.nodes[2]));
+        assertEquals(unionFind.find(unionFind.nodes[0]), unionFind.find(unionFind.nodes[1]));
     }
 
 }
